7bf5ec535496e9ab59305cc31c6288ebdab98065
isma-91 <isma91@hotmail.it>
2023-01-13 15:22:45 +0100

    first commit

---
+++ config/menu.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    [
+        'route' => 'home',
+        'label' => 'DC',
+    ],
+    [
+        'route' => 'comics',
+        'label' => 'COMICS',
+    ],
+    [
+        'route' => 'movies',
+        'label' => 'MOVIES',
+    ],
+];
